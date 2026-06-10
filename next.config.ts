b89d16bec7948de43dabcd5fc3ae764414125bf1
hakhakop <hakop28ol@yahoo.com>
2026-06-10 14:10:10 +0400

    config ts update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const wordpressUrl = new URL(
+  process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL ||
+    process.env.WORDPRESS_SITE_URL ||
+    "https://cms.webpages.am"
+);
+
+const wordpressUploadsPath = `${wordpressUrl.pathname.replace(
+  /\/$/,
+  ""
+)}/wp-content/uploads/**`;
+
 const nextConfig = {
   devIndicators: false,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cms.webpages.am",
-      },
-      {
-        protocol: "https",
-        hostname: "webpages.am",
+        protocol: wordpressUrl.protocol.replace(":", ""),
+        hostname: wordpressUrl.hostname,
+        pathname: wordpressUploadsPath,
       },
       {
         protocol: "https",
